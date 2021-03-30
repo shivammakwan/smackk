c934a409f9025c5c3e993e92d9e0dc1b9646dc04
shivam makwan <shivammakwan124@gail.com>
2021-03-30 22:38:29 +0530

    import fix

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Avatar, Button, IconButton } from "@material-ui/core";
-import ChatIcon from "@material-ui/icons/chat";
+import ChatIcon from "@material-ui/icons/Chat";
 import MoreIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
 import * as EmailValidator from "email-validator";
